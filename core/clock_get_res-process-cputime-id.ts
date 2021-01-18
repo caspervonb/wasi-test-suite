49c6f3c9a0acb14bda9d7843381535f86687c249
Casper Beyer <caspervonb@pm.me>
2021-01-18 02:43:14 +0000

    Update core to 34eb66d

---
+++ core/clock_get_res-process-cputime-id.ts
@@ -0,0 +1,11 @@
+import {
+  clock_res_get,
+  clockid,
+  errno,
+} from "wasi";
+
+const err = clock_res_get(clockid.PROCESS_CPUTIME_ID, 0);
+assert(err == errno.SUCCESS);
+
+const res = load<u64>(0);
+assert(res > 0);
